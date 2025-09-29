731d5f483eaa71326bdd6912e99d6f0379878c59
Djblaik <blaikey@outlook.com>
2025-09-29 11:31:25 +1000

    add gracefull shutdown

--- python/microphone.py
+++ python/microphone.py
@@ -1,8 +1,8 @@
-
 import time
 import numpy as np
 import pyaudio
 import config
+import off
 
 
 def start_stream(callback):
@@ -15,17 +15,26 @@ def start_stream(callback):
                     frames_per_buffer=frames_per_buffer)
     overflows = 0
     prev_ovf_time = time.time()
-    while True:
-        try:
-            y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
-            y = y.astype(np.float32)
-            stream.read(stream.get_read_available(), exception_on_overflow=False)
-            callback(y)
-        except IOError:
-            overflows += 1
-            if time.time() > prev_ovf_time + 1:
-                prev_ovf_time = time.time()
-                print('Audio buffer has overflowed {} times'.format(overflows))
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+
+    try:
+        while True:
+            try:
+                y = np.frombuffer(
+                    stream.read(frames_per_buffer, exception_on_overflow=False),
+                    dtype=np.int16
+                ).astype(np.float32)
+                stream.read(stream.get_read_available(), exception_on_overflow=False)
+                callback(y)
+            except IOError:
+                overflows += 1
+                if time.time() > prev_ovf_time + 1:
+                    prev_ovf_time = time.time()
+                    print(f'Audio buffer has overflowed {overflows} times')
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, stopping stream...")
+    finally:
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        off.off()
+
